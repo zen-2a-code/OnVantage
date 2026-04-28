8a96f4bce384b1ac28e2e8a8fdaaf40a09ab6d39
Stoyan Hristov <stoioxristov@gmail.com>
2026-04-28 11:40:44 +0300

    inital commit

---
+++ OnVantage/OnVantageApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnVantageApp.swift
+//  OnVantage
+//
+//  Created by Stoyan Hristov on 28.04.26.
+//
+
+import SwiftUI
+
+@main
+struct OnVantageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
