474312e90207183360c55598603cf1c5de78de54
Stoyan Hristov <stoioxristov@gmail.com>
2026-04-30 00:13:25 +0300

    adding guard let instead of if let to avoid multiple level of indentation and harder to read code; improving the error logging

--- OnVantage/Services/SeedImporter.swift
+++ OnVantage/Services/SeedImporter.swift
@@ -13,56 +13,57 @@ enum SeedImporter {
         guard !UserDefaults.standard.bool(forKey: "didSeedV1") else { return }
 
         let jsonDecoder = JSONDecoder()
-        
-        if let seedURL = Bundle.main.url(
-            forResource: "seed_swiftui",
-            withExtension: "json"
-        ) {
-            if let seedData = try? Data(contentsOf: seedURL) {
-                if let seedCategoryDTO = try? jsonDecoder.decode(
-                    SeedCategoryDTO.self,
-                    from: seedData
-                ) {
-                    let swiftCategory = Category(
-                        name: seedCategoryDTO.name,
-                        colorHex: seedCategoryDTO.colorHex,
-                        iconName: seedCategoryDTO.iconName,
-                        isActive: true,
-                        isUserCreated: false,
-                        createdAt: .now
-                    )
 
-                    var challengesList = [Challenge]()
-                    var challengesUUIDS = Array<UUID>()
-                    for seedCategoryChallenge in seedCategoryDTO.challenges {
-                        let challenge = Challenge(
-                            title: seedCategoryChallenge.title,
-                            conceptExplanation: seedCategoryChallenge.conceptExplanation,
-                            taskDescription: seedCategoryChallenge.taskDescription,
-                            difficulty: seedCategoryChallenge.difficulty,
-                            isUserCreated: false,
-                            createdAt: .now,
-                            category: swiftCategory
-                        )
-                        
-                        challengesUUIDS.append(challenge.id)
-                        challengesList.append(challenge)
-                    }
-                    
-                    swiftCategory.challenges = challengesList
-                    
-                    let categoryProgress = CategoryProgress(category: swiftCategory, cycleStartedAt: .now)
-                    categoryProgress.shuffledOrder = challengesUUIDS.shuffled()
-                    swiftCategory.progress = categoryProgress
-                    
-                    context.insert(swiftCategory)
-                    UserDefaults.standard.set(true, forKey: "didSeedV1")
-                    return
-                }
-            }
+        guard let seedURL = Bundle.main.url(forResource: "seed_swiftui",withExtension: "json") else {
+            fatalError("Missing seed JSON file")
         }
-
-        fatalError("Unable to load seed data from JSON")
+        
+        guard let seedData = try? Data(contentsOf: seedURL) else {
+            fatalError("Could not read seed file")
+        }
+        
+        guard let seedCategoryDTO = try? jsonDecoder.decode(SeedCategoryDTO.self, from: seedData) else {
+            fatalError("Could not decode seed JSON")
+        }
+        
+        let swiftCategory = Category(
+            name: seedCategoryDTO.name,
+            colorHex: seedCategoryDTO.colorHex,
+            iconName: seedCategoryDTO.iconName,
+            isActive: true,
+            isUserCreated: false,
+            createdAt: .now
+        )
+        
+        var challengesList = [Challenge]()
+        var challengesUUIDS = [UUID]()
+        for seedCategoryChallenge in seedCategoryDTO.challenges {
+            let challenge = Challenge(
+                title: seedCategoryChallenge.title,
+                conceptExplanation: seedCategoryChallenge
+                    .conceptExplanation,
+                taskDescription: seedCategoryChallenge.taskDescription,
+                difficulty: seedCategoryChallenge.difficulty,
+                isUserCreated: false,
+                createdAt: .now,
+                category: swiftCategory
+            )
+            
+            challengesUUIDS.append(challenge.id)
+            challengesList.append(challenge)
+        }
+        
+        swiftCategory.challenges = challengesList
+        
+        let categoryProgress = CategoryProgress(
+            category: swiftCategory,
+            cycleStartedAt: .now
+        )
+        categoryProgress.shuffledOrder = challengesUUIDS.shuffled()
+        swiftCategory.progress = categoryProgress
+        
+        context.insert(swiftCategory)
+        UserDefaults.standard.set(true, forKey: "didSeedV1")
     }
 }
 
