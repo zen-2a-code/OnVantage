1096acaa2e22087fda1a54d831dce232a8463a83
Stoyan Hristov <stoioxristov@gmail.com>
2026-05-14 14:16:37 +0300

    Creating a  ChallengeDetailsView - no VVM yet for Home and ChallengeDetails Views.

---
+++ OnVantage/Views/Home/ChallengeDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeDetailsView.swift
+//  OnVantage
+//
+//  Created by Stoyan Hristov on 13.05.26.
+//
+
+import SwiftUI
+
+struct ChallengeDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallengeDetailsView()
+}
